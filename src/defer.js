7735e017c1f14268aca8a6311bc3c65d5c74bed2
Ian Wessman <github@iria.net>
2012-01-23 00:48:03 -0500

    Saved 0.01kB off the gzipped output by adjusting license whitespace

--- src/defer.js
+++ src/defer.js
@@ -4,22 +4,21 @@
 // ==/ClosureCompiler==
 
 /** @license
- *  
  * "defer.js" is a trademark of Ian J. Wessman
- *  
- *  Copyright 2011-2012 Ian J. Wessman
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Copyright 2011-2012 Ian J. Wessman
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 /*  // This error struct exists for developer documentation,
